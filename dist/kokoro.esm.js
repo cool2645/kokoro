1b4410a5dacec4715621aae0d92f8ba221fb2aee
Riko Sakurauchi <lijiahao99131@gmail.com>
2019-09-08 11:29:53 +0800

    0.1.0-beta.3

--- dist/kokoro.esm.js
+++ dist/kokoro.esm.js
@@ -1,7 +1,7 @@
 /*!
  * kokoro - Headless music player written with Redux.
  * --------
- * @version 0.1.0-beta.1
+ * @version 0.1.0-beta.3
  * @homepage: https://github.com/cool2645/kokoro#readme
  * @license MIT
  * @author rikakomoe
@@ -102,7 +102,7 @@ function _nonIterableSpread() {
   throw new TypeError("Invalid attempt to spread non-iterable instance");
 }
 
-var version = "0.1.0-beta.1";
+var version = "0.1.0-beta.3";
 
 var Song =
 /*#__PURE__*/
